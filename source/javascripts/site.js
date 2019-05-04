24360bb693c6f23570a1d47374d7d20da4161d43
Brian Kelly <polymonic@gmail.com>
2019-05-04 08:31:58 -0400

    Sound clips are now just big buttons

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
-// This is where it all goes :)
+// From https://github.com/CreateJS/SoundJS/blob/master/examples/02_PlayOnClick.html
+
+function init(sounds) {
+    createjs.defaultInterruptBehavior = createjs.Sound.INTERRUPT_ANY;
+    createjs.Sound.registerSounds(sounds, "/sounds/", 0);
+}
+
+function playSound(soundID) {
+    createjs.Sound.play(soundID);
+}
+
+function stopSounds() {
+    createjs.Sound.stop();
+}
